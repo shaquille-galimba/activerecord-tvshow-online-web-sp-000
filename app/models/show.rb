e5f08c6134fb3e91a6007243800237ef9187055c
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-04-02 05:49:26 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.order(rating: :desc).first
+    self.where("rating = ?", self.highest_rating).first
   end
 
   def self.lowest_rating
